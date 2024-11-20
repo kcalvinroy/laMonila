cbd218eadee2c9d94988518767a5d7a22b353c89
kcalvinroy <kcalvinroy@gmail.com>
2024-11-20 18:42:08 +0300

    category title using better logic

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -49,7 +49,11 @@ export default async function CategoryPage({
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4  sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <div className="flex justify-between items-center">
-            <h2 className="text-2xl font-bold tracking-tight text-gray-900"></h2>
+            <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+              {(await params).category === "all"
+                ? "All Our Products"
+                : `Our Products of ${(await params).category}`}
+            </h2>
           </div>
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {data.map((product) => (
