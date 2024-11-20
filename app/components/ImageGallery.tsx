28a990dcd3352772990f76540adf50b28869e052
kcalvinroy <kcalvinroy@gmail.com>
2024-11-20 17:05:37 +0300

    Added specific interface for ImageGallery.tsx

--- app/components/ImageGallery.tsx
+++ app/components/ImageGallery.tsx
@@ -26,7 +26,7 @@ export default function ImageGallery({ images }: iAppProps) {
   return (
     <div className="grid gap-4 lg:grid-cols-5">
       <div className="order-last flex gap-4 lg:order-none lg:flex-col">
-        {images.map((image: any, idx: any) => (
+        {images.map((image: SanityImage, idx: number) => (
           <div key={idx} className="overflow-hidden rounded-lg bg-gray-100">
             <Image
               src={urlFor(image).url()}
