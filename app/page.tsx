87b21ae469040fa01b1e8e5d2efe972bef85f994
kcalvinroy <kcalvinroy@gmail.com>
2024-12-02 23:49:26 +0300

    Added Better Navigation and Drop downs

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,12 @@ import Categories from "./components/Categories";
 import Hero from "./components/Hero";
 import Newest from "./components/Newest";
 import About from "./components/About";
-import NavCategoriesData from "./components/NavCategoriesData";
 
 export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
     <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
-      {/* <NavCategoriesData /> */}
       <Hero />
       <About />
       <Categories />
